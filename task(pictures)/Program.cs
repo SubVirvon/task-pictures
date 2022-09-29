1662189c79812fdd22760b3e19c5802ab428d17a
Virvon <dan130205@gmail.com>
2022-09-29 20:41:23 +0300

    task pictures

---
+++ task(pictures)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_pictures_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount = 52, rowLenght = 3, rowCount, freePictures;
+
+            rowCount = picturesCount / rowLenght;
+            freePictures = Convert.ToInt32(Convert.ToSingle(picturesCount) % rowLenght);
+            Console.WriteLine("Количество заполненных рядов: " + rowCount + "\nКоличество оставшихся картинок: " + freePictures);
+            Console.ReadKey();
+        }
+    }
+}
